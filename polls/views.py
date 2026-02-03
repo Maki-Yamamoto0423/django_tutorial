aec0525238e34be4b1270aa181b5b2dcc841916e
Maki-Yamamoto0423 <maki.yamamoto@anthea.co.jp>
2026-02-03 13:31:02 +0900

    pollsアプリのURL設定とビューを追加

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("You're looking at polls.")
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
