156fc757a674ef80dc80b2beff1214b7e0ff9d55
Maki-Yamamoto0423 <maki.yamamoto@anthea.co.jp>
2026-02-03 14:54:43 +0900

    pollsアプリに投票機能を追加し、結果表示用のビューとURLを設定

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
