e640aa18bd6d8582531932737563a6ba5a99548e
Maki-Yamamoto0423 <maki.yamamoto@anthea.co.jp>
2026-02-04 17:35:33 +0900

    Questionモデルに最近公開されたかどうかを判断するメソッドを追加し、テストケースを実装

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.utils import timezone
+import datetime
 
-# Create your tests here.
+from .models import Question
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        pub_date が未来の日付の場合、
+        was_published_recently() は False を返すべき。
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
