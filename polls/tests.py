395c885cec09c1c8110932534d27181339300bef
Maki-Yamamoto0423 <maki.yamamoto@anthea.co.jp>
2026-02-05 11:25:31 +0900

    テストケースを追加し、was_published_recently() メソッドの動作を検証

--- polls/tests.py
+++ polls/tests.py
@@ -1,6 +1,7 @@
+import datetime
+
 from django.test import TestCase
 from django.utils import timezone
-import datetime
 
 from .models import Question
 
@@ -13,3 +14,21 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        pub_date が1日より前の場合、
+        was_published_recently() は False を返す
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        pub_date が直近1日以内の場合、
+        was_published_recently() は True を返す
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
