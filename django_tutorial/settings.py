eaaff416406018a4178b05dbdd99d978c6713ecf
Maki-Yamamoto0423 <maki.yamamoto@anthea.co.jp>
2026-02-06 14:34:51 +0900

    django-polls アプリケーションのテンプレートを追加し、設定を更新。不要なパッケージのコメントアウトとURL設定を修正。

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
-    'polls',          # 既存アプリ
-    'django_polls',   # 新しいパッケージ
+    'polls',
+    # 'django_polls',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'django_tutorial.urls'
--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
 
 urlpatterns = [
-    path('polls/', include('django_polls.urls')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
